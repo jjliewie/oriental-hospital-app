cdbde7c6c7c746de4487efbe89b72df1fd752448
jjliewie <rheej@ismanila.org>
2022-01-01 15:53:08 +0800

    Initial Commit

---
+++ commission-draft/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  commission-draft
+//
+//  Created by Julie Rhee on 1/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ commission-draft/commission_draftApp.swift
@@ -0,0 +1,17 @@
+//
+//  commission_draftApp.swift
+//  commission-draft
+//
+//  Created by Julie Rhee on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct commission_draftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
