d6ba66d08ddf5280a8bebf3be00152faae8cc457
jjliewie <rheej@ismanila.org>
2022-01-23 10:47:49 +0800

    location view

--- commission-draft/LocationView.swift
+++ commission-draft/LocationView.swift
@@ -26,10 +26,26 @@ struct LocationView: View {
     @Environment(\.presentationMode) var presentationMode
     
     @State private var searchText = "";
+    
+    @State private var medicines_chosen: Array<String> = [];
+    
+    func make_list(items: [info]) -> Array<String>{
+        var list: Array<String> = []
+        
+        for item in items{
+            for i in item.medicines.filter({!list.contains($0)}){
+                list.append(i)
+            }
+        }
+        
+        return list
+    }
 
     var body: some View {
         
         ZStack{
+            
+            let medicine_list = make_list(items: getJson())
             
             VStack{
                 
@@ -38,9 +54,38 @@ struct LocationView: View {
                 SearchBar(text: $searchText, initialText: "한의학을 입력해주세요...")
                     .padding()
                 
-                if !searchText.isEmpty{
-                    Text("hello world")
+                if(!medicines_chosen.isEmpty){
+
+                    ScrollView(.horizontal){
+
+                        HStack{
+
+                            ForEach(medicines_chosen, id: \.self){ medicine in
+                                ChosenItem(text: medicine, items: $medicines_chosen)
+                            }
+
+                        }
+                        .padding(.bottom, 10)
+
+                    }.padding(.horizontal, 40)
+
                 }
+                
+                if !searchText.isEmpty{
+                    
+                    ForEach(medicine_list.filter({"\($0)".contains(searchText) && !medicines_chosen.contains("\($0)")}), id: \.self){ item in
+                        
+                        Button(action:{
+                            medicines_chosen.append(item)
+                        }){
+                            Text(item)
+                        }
+                        
+                    }// foreach
+                }
+                
+                location_scroll(items: getJson(), chosen: medicines_chosen)
+                
                 
                 Button(action: {
                     presentationMode.wrappedValue.dismiss()
@@ -64,6 +109,29 @@ struct LocationView: View {
             
         } // ZStack
         
+    } // body
+} // view
+
+struct location_scroll: View{
+    
+    var items: [info]
+    var chosen: Array<String>
+    
+    var body: some View{
+        ScrollView{
+            LazyVGrid(columns: [
+                GridItem(.flexible(minimum: 50, maximum: 200), spacing: 25, alignment: .top),
+                GridItem(.flexible(minimum: 50, maximum: 200), spacing: 25),
+            ], alignment: .leading, spacing: 25, content: {
+                
+                ForEach(items.filter({Set(chosen).isSubset(of: Set($0.medicines))}), id: \.self){ type in
+                    
+                    Text(type.name)
+                }
+            }) // lazyvgrid
+            .padding(25)
+            .padding(.horizontal)
+        } // scroll
     } // body
 } // view
 
